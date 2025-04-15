3b386161a79ccdb75a9e940cc8b8e17a052897f6
Techwin Labs LLP <techwinlabs@gmail.com>
2025-04-15 10:55:04 +0530

    feat:added swipe counts rpx method

--- src/components/SwipesCount/index.jsx
+++ src/components/SwipesCount/index.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { supabase } from "../../utils";
 
 const SwipesCounter = () => {
+  const [count, setCount] = useState(0);
+  const fetchSwipeCount = async () => {
+    const { data, error } = await supabase.rpc("count_liked_songs");
+    if (data && !error) setCount(data);
+  };
+
+  useEffect(() => {
+    fetchSwipeCount();
+  }, []);
   return (
     <div className="bg-[#F9F0FF] py-[40px] max-mlg:py-[20px] ">
       <div className="px-[20px] max-w-[1600px] mx-auto">
         <div className="px-[90px] py-[20px] bg-[#FFF] max-w-[380px] rounded-[32px] mx-auto max-md:px-[20px]">
           <h2 className="text-[32px] text-[#8A4DFF] text-center font-bold">
-            115,072,250
+            {count}
           </h2>
           <p className="text-center font-semibold mt-[10px]">
             Swipes on NewTunes
