43b1f85c73f5b3601bc0d54c6ac497074a5feba7
Techwin Labs LLP <techwinlabs@gmail.com>
2025-04-15 15:50:47 +0530

    code updated

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 
 const Layout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div>
       <Header />
